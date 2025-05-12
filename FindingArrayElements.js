631bb1c39e48787b4084f3723b243f0451e9c074
toxidity-18 <desavigatunganga@gmail.com>
2025-05-12 04:39:30 +0300

    anything

--- FindingArrayElements.js
+++ FindingArrayElements.js
@@ -1,5 +1,10 @@
 const cards = ['queen of hearts', 'jack of clubs', 'ten of diamonds', 'ace of spades']
+
+// using simple conditions
 cards.indexOf('jack of clubs') // 1
 cards.indexOf('ace of spades') // 3
 cards.indexOf('ace of spades',2) // 3
 cards.indexOf('ten of diamonds',3)// -1
+
+// using complex conditions Array.prototype.find()
+// pass it as a callback function 
