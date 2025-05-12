9bb3bc9f47e72332bbb424b2b3000b0a0368c8fe
toxidity-18 <desavigatunganga@gmail.com>
2025-05-12 04:01:36 +0300

    learning a few things here and there

---
+++ FindingArrayElements.js
@@ -0,0 +1,5 @@
+const cards = ['queen of hearts', 'jack of clubs', 'ten of diamonds', 'ace of spades']
+cards.indexOf('jack of clubs') // 1
+cards.indexOf('ace of spades') // 3
+cards.indexOf('ace of spades',2) // 3
+cards.indexOf('ten of diamonds',3)// -1
